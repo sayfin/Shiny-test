12d0941d521e827a12b6957fd2a9addc32ae7072
Hamada <Sayfeddine.Hamada@originenergy.com.au>
2015-11-11 12:16:16 +1100

    Sayf's Panels

--- ui.R
+++ ui.R
@@ -1,40 +1,30 @@
 library(shiny)
 
-shinyUI(fluidPage(
-
-  titlePanel("Log Returns"),
-  
- sidebarLayout(
-    sidebarPanel(
-     sliderInput("bins",
+shinyUI(navbarPage("Oil Model", tabPanel("Historical data",
+   sidebarLayout(
+   sidebarPanel(
+   sliderInput("bins", 
                   "Number of bins:",
                   min = 1,
                   max = 50,
                   value = 30)
     ),
-    
     mainPanel(
-      plotOutput("distPlot")
-    )
- ),
-    
- fluidRow(
-   
-   column(3,
-          #h3("Buttons"),
-          actionButton("action", label = "Action"),
-          br(),
-          br(), 
-          submitButton("Submit")),
-   column(3,
-          h3("Distribution fitting"),
-          checkboxInput("checkbox", label = "Normal", value = TRUE)),
-   column(3,
-          #h2("Commodity"), 
-          selectInput("selection", label = "Commodity", choices = c("Brent", "FX"), selected = "Brent")
-          )
-    ) 
-)
-)
-
-
+    plotOutput("distPlot")
+    )),
+   fluidRow(
+     column(3,
+            #h3("Buttons"),
+            actionButton("action", label = "Action"),
+            br(),
+            br(), 
+            submitButton("Submit")),
+     column(3,
+            h3("Distribution fitting"),
+            checkboxInput("checkbox", label = "Normal", value = TRUE)),
+     column(3,
+            #h2("Commodity"), 
+            selectInput("selection", label = "Commodity", choices = c("Brent", "FX"), selected = "Brent"))
+   )),
+tabPanel("Stats"), navbarMenu("Simulation", tabPanel("Brent"), tabPanel("FX"))
+))
